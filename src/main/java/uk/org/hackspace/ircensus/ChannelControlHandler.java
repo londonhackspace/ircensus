25092bcf183ce7b318774a561a42145d12e48223
Elliot West <teabot@gmail.com>
2011-01-19 10:32:42 +0000

    Fixed handler issue. More logging

--- src/main/java/uk/org/hackspace/ircensus/ChannelControlHandler.java
+++ src/main/java/uk/org/hackspace/ircensus/ChannelControlHandler.java
@@ -5,51 +5,62 @@ import org.slf4j.LoggerFactory;
 
 class ChannelControlHandler extends AbstractIrcHandler {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ChannelControlHandler.class);
+  private static final Logger LOG = LoggerFactory.getLogger(ChannelControlHandler.class);
 
-    public ChannelControlHandler(IrcClient ircClient) {
-        super(ircClient);
-    }
+  private static final String JOIN_COMMAND = "join";
+  private static final String LEAVE_COMMAND = "leave";
 
-    @Override
-    public String executeCommand(String method, String[] tokens, String sender) {
-        String response = null;
-        if ("join".equals(method)) {
-            response = commandJoinChannel(tokens);
-        } else if ("leave".equals(method)) {
-            response = commandLeaveChannel(tokens);
-        }
-        return response;
-    }
+  public ChannelControlHandler(IrcClient ircClient) {
+    super(ircClient);
+  }
 
-    private String commandJoinChannel(String[] tokens) {
-        String response;
-        if (tokens.length == 1) {
-            ircClient.joinChannel(tokens[0]);
-            response = "Joined channel: " + tokens[0];
-            LOG.info(response);
-        } else if (tokens.length == 2) {
-            ircClient.joinChannel(tokens[0], tokens[1]);
-            response = "Joined channel: " + tokens[0];
-            LOG.info(response);
-        } else {
-            response = "Usage: join <channel> [<password>]";
-            LOG.debug(response);
-        }
-        return response;
+  @Override
+  public String executeCommand(String method, String[] tokens, String sender) {
+    String response = null;
+    if (JOIN_COMMAND.equals(method)) {
+      response = commandJoinChannel(tokens);
+    } else if (LEAVE_COMMAND.equals(method)) {
+      response = commandLeaveChannel(tokens);
     }
+    return response;
+  }
 
-    private String commandLeaveChannel(String[] tokens) {
-        String response;
-        if (tokens.length == 1) {
-            ircClient.partChannel(tokens[0]);
-            response = "Left channel: " + tokens[0];
-            LOG.info(response);
-        } else {
-            response = "Usage: leave <channel>";
-            LOG.debug(response);
-        }
-        return response;
+  private String commandJoinChannel(String[] tokens) {
+    String response;
+    if (tokens.length == 2) {
+      String channelName = tokens[1];
+      LOG.info("Joining channel '{}' ...", channelName);
+      ircClient.joinChannel(channelName);
+      response = "Joined channel: " + channelName;
+      LOG.info(response);
+    } else if (tokens.length == 3) {
+      String channelName = tokens[1];
+      String password = tokens[2];
+      LOG.info("Joining locked channel '{}' ...", channelName);
+      ircClient.joinChannel(channelName, password);
+      response = "Joined channel '" + channelName + "'";
+      ;
+      LOG.info(response);
+    } else {
+      response = "Usage: join <channel> [<password>]";
+      LOG.debug(response);
     }
+    return response;
+  }
+
+  private String commandLeaveChannel(String[] tokens) {
+    String response;
+    if (tokens.length == 2) {
+      String channelName = tokens[1];
+      LOG.info("Leaving channel '{}' ...", channelName);
+      ircClient.partChannel(channelName);
+      response = "Left channel '" + channelName + "'";
+      LOG.info(response);
+    } else {
+      response = "Usage: leave <channel>";
+      LOG.debug(response);
+    }
+    return response;
+  }
 
 }
